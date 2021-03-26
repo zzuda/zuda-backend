4ea8898e8162458c6e977f04ac9f3ec6e85571bc
DaegyeomHa <combbm@naver.com>
2021-03-26 23:56:52 +0900

    fix: delete unnessecary files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService]
+  imports: [ConfigModule.forRoot()]
 })
 export class AppModule {}
