454f073acad6c2208e33cc7bbc46840de785b2ff
DaegyeomHa <combbm@naver.com>
2021-03-27 00:16:13 +0900

    fix: apply ConfigService instead of process.env

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = app.get<ConfigService>('ConfigService');
+
+  const corsHost = config.get<string>('CORS_HOST', 'http://localhost:3000');
 
   app.enableCors({
-    origin: process.env.CORS_HOST || 'localhost:3000'
+    origin: corsHost
   });
 
   await app.listen(8080);
 }
+
 bootstrap();
