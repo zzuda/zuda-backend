1d85c0a094a62c945dd8bc1d7869c55ae1f88be1
DaegyeomHa <combbm@naver.com>
2021-03-26 23:34:30 +0900

    feat: nestjs init

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 module.exports = {
     parser: '@typescript-eslint/parser',
     extends: ['plugin:@typescript-eslint/recommended', 'airbnb-base', 'eslint-config-prettier'],
+    env: {
+      node: true,
+      jest: true,
+    },
     rules: {
       'arrow-body-style': 'off',
       'comma-dangle': 'off',
@@ -9,6 +13,18 @@ module.exports = {
       'import/extensions': 'off',
       'linebreak-style': 'off',
       'import/prefer-default-export': 'off',
+      'import/no-extraneous-dependencies': [ 
+        'error', {
+          'devDependencies': [
+            '**/*.test.ts',
+            '**/*.spec.ts',
+            '**/*.e2e-spec.ts'
+          ]
+        }
+      ],
+      'class-methods-use-this': 'off',
+      'no-useless-constructor': 'off',
+      'no-empty-function': 'off',
 
       'no-shadow': 'off',
       '@typescript-eslint/no-shadow': ['error'],
