9bf33cdbf333f57c7444e2e4236a46c07ea802fc
DaegyeomHa <combbm@naver.com>
2021-03-27 00:25:50 +0900

    feat: apply SequelizeModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [ConfigModule.forRoot()]
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    SequelizeModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        dialect: 'mysql',
+        host: config.get<string>('DATABASE_HOST', 'localhost'),
+        port: config.get<number>('DATABASE_PORT', 3306),
+        username: config.get<string>('DATABASE_USERNAME', 'root'),
+        password: config.get<string>('DATABASE_PASSWORD', ''),
+        database: config.get<string>('DATABASE_DB', 'zuda'),
+        models: []
+      })
+    })
+  ]
 })
 export class AppModule {}
