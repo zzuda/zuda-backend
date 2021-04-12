41a7e496cd8267f2ee771b2cbae251e5f4e32bc9
DaegyeomHa <combbm@naver.com>
2021-04-13 00:03:48 +0900

    feat: 유저 중복 체크 및 비밀번호 암호화 구현

--- src/shared/errors/user.error.ts
+++ src/shared/errors/user.error.ts
@@ -2,7 +2,11 @@ import { ErrorInfo } from 'src/types';
 
 export const UserError: ErrorInfo = {
   USER_NOT_FOUND: {
-    code: 'USER-404',
-    message: '존재하지 않는 유저입니다.'
+    code: 'user-404',
+    message: '존재하지 않는 계정입니다.'
+  },
+  USER_ALREADY_EXISTS: {
+    code: 'user-409',
+    message: '이미 존재하는 계정입니다.'
   }
 };
