b1fb5036b46de795c5633688b64a6a3f915c8dfb
DaegyeomHa <combbm@naver.com>
2021-04-18 19:01:21 +0900

    refactor: move jwt.strategy.ts to strategies directory

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -5,7 +5,7 @@ import { PassportModule } from '@nestjs/passport';
 import { UserModule } from 'src/user/user.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
-import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
   imports: [
