f73e2de8d3328190ca2e110e8a525f28e7bc45b5
DaegyeomHa <combbm@naver.com>
2021-04-22 16:23:37 +0900

    fix: add versionKey option
    __v column을 사용하지 않기 위해 옵션 추가

--- src/room/room.schema.ts
+++ src/room/room.schema.ts
@@ -1,7 +1,9 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { Document } from 'mongoose';
 
-@Schema()
+@Schema({
+  versionKey: false
+})
 export class RoomMember {
   @Prop()
   roomId!: number;
