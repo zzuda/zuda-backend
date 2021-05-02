8cfa5a523bcb9d9c28bd94912aee44ca47d8b557
HARDTACK-Dev <rjsdn827@gmail.com>
2021-05-02 16:30:07 +0900

    chore: IsOptional keyword added

--- src/file/dto/upload-file-body.dto.ts
+++ src/file/dto/upload-file-body.dto.ts
@@ -1,6 +1,14 @@
-import { IsNumberString } from 'class-validator'
+import { IsNumberString, IsOptional, IsString } from 'class-validator'
 
 export class FileBodyDTO{
     @IsNumberString()
     roomID!: number;
+
+    @IsString()
+    @IsOptional()
+    fileName?: string;
+
+    @IsString()
+    @IsOptional()
+    Files?: []
 }
