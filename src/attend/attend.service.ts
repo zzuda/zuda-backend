ea5bcb863f31d78ff72af9e25f43271c29f088b4
HARDTACK-Dev <rjsdn827@gmail.com>
2021-05-29 23:26:33 +0900

    chore: eslint
    - 수정할 예정

--- src/attend/attend.service.ts
+++ src/attend/attend.service.ts
@@ -1,68 +1,55 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { AttendError } from 'src/shared/errors/attendance.error';
-import { CreatedWordReturn } from 'src/types';
+import {Injectable, NotFoundException, ConflictException} from '@nestjs/common';
+import {AttendError} from 'src/shared/errors/attendance.error';
+import {CreatedWordReturn} from 'src/types';
 
-import { WordService } from '../word/word.service';
+import {WordService} from '../word/word.service';
+import {RoomService} from '../room/services/room.service'
 
-import { AttendBodyDTO } from './dto/attend-body.dto';
+import {AttendBodyDTO} from './dto/attend-body.dto';
 
 @Injectable()
 export class AttendService {
-  constructor(private readonly wordService: WordService) {}
+    constructor(
+        private readonly wordService : WordService,
+        private readonly roomService : RoomService
+    ) {}
 
-  // this method return room's Randowords or Quiz
-  async createWord(attendBodyDTO: AttendBodyDTO): Promise<CreatedWordReturn> {
-    const { randomCount, type } = attendBodyDTO;
+    // this method return room's Randowords or Quiz
+    async createWord(attendBodyDTO : AttendBodyDTO): Promise<CreatedWordReturn> {
+        const {roomID ,randomCount, type} = attendBodyDTO;
 
-    
-    const roomsRandomWords: string[] = [];
+        if (type === 'word') {
 
-    if (type === 'word') {
+          if(await this.roomService.existsRoom(roomID) === true){
+              AttendError.
+          }
 
-    
-    const promises = [];
-    // eslint-disable-next-line no-plusplus
-    for (let i = 1; i <= randomCount; i++) {
-      promises.push(this.wordService.makeRandomWord());
+            const Words = [];
+            // eslint-disable-next-line no-plusplus
+            for (let i = 1; i <= randomCount; i++) {
+                Words.push(this.wordService.makeRandomWord());
+            }
+
+            const results = await Promise.all(Words);
+
+            return {
+              message: '초대 코드가 생성되었습니다.', 
+              count: randomCount, 
+              words: results};
+        }
+
+        if (type === 'quiz') {
+            // do something
+
+            return {message: '초대 코드가 생성되었습니다.', count: randomCount, words: roomsRandomWords};
+        }
+        if (type !== 'quiz' && type !== 'word') {
+            throw new NotFoundException(AttendError.WRONG_ATTENDANCE_TYPE);
+        }
+        return {
+          message: "what should I do?", 
+          count: 0, 
+          words: []
+        }
     }
-
-    const results = await Promise.all(promises);
-
-    // eslint-disable-next-line no-console
-    // console.log(results);
-    
-    
-      // roomsRandomWords = promises.slice();
-      
-      // eslint-disable-next-line no-console
-      // console.log(roomsRandomWords);
-
-    
-      
-
-      return {
-        message: '초대 코드가 생성되었습니다.',
-        count: randomCount,
-        words: results
-      };
-    }
-
-    if (type === 'quiz') {
-      // do something
-
-      return {
-        message: '초대 코드가 생성되었습니다.',
-        count: randomCount,
-        words: roomsRandomWords
-      };
-    }
-    if (type !== 'quiz' && type !== 'word') {
-      throw new NotFoundException(AttendError.WRONG_ATTENDANCE_TYPE);
-    }
-    return {
-        message: "what should I do?",
-        count: 0,
-        words: []
-    }
-  }
 }
--- src/shared/errors/attendance.error.ts
+++ src/shared/errors/attendance.error.ts
@@ -5,4 +5,8 @@ export const AttendError: ErrorInfo = {
     code: 'attend-404',
     message: '해당 attendance 타입은 존재하지 않습니다.'
   },
+  ROOM_ALREADY_EXIST: {
+    code: 'attend-409',
+    message: '해당 방의 초대 코드가 이미 생성되어 있습니다.'
+  },
 };
